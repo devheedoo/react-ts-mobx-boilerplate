6dd42e01acc9ae03ddeef151c99aff42dac3bd37
Heedo Kim(Choco) on Macbook <devheedoo@gmail.com>
2020-01-17 16:33:01 +0900

    Add MobX

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import {computed, observable, reaction, action} from 'mobx';
+import {inject, observer} from 'mobx-react';
 
-const App = () => {
-  return (
-    <>
-      <SayHi name={"Choco"} />
-      <SayHi name={"Vanilla"} />
-    </>
-  );
+@observer
+class App extends React.Component {
+  @observable names: string[] = ['Choco', 'Vanilla', 'Strawberry'];
+
+  @action
+  addSomebody = () => {
+    this.names
+    ? this.names.push(`Somebody${this.names.length}`)
+    : this.names = [`Choco`];
+  }
+
+  @action
+  removeAllNames = () => {
+    if (this.names) this.names = null;
+  }
+
+  componentDidMount() {
+    reaction(
+      () => this.names?.length,
+      () => {
+        console.log(this.names);
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addSomebody}>Add Somebody</button>
+        <button onClick={this.removeAllNames}>Remove All Names</button>
+        {this.names?.map(name => {
+          return (
+            <SayHi key={name} name={name} />
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 interface SayHiProps {
